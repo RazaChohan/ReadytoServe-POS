06274ace72d083e7498d1440f46f8a73adb330d9
Muhammad Raza <muhammad.raza@coeus-solutions.de>
2014-09-15 15:11:39 +0500

    Add Code Prototype of Oauth Controller Class Contains: Functions for Oauth Controller Class Short Description, access level, return and parameter of functions Data Members of Controller and their description

---
+++ code/controllers/OauthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ready2toServe\Controller\OauthController;
+/**
+ * Contains definition of Oauth Controller class namely OauthController
+ * 
+ * This file contains one class implementation
+ * named OauthController class.
+ *
+ * @category Training/Learning PHP
+ * @package Ready2Serve
+ * @version v 1.0
+ */
+/**
+ * Contains Oauth Controller class
+ *
+ * This Class gets a specific request from the Dispatcher class
+ * and performs functions according to the request.
+ * 
+ * @package Ready2Serve
+ * @author Muhammad Raza <muhammad.raza@coeus-solutions.de>
+ * @category Training/Learning PHP
+ * @version v 1.0
+ */
+class OauthController
+{
+    /**
+     * @var view 'View class object'
+     */
+    public $view;
+    /**
+     * @var object 'Object of Order Model Class'
+     */
+    private $personModel;
+    /**
+     * @var Boolean 'Is user Authenticated'
+     */
+    private $IsAuthenicated;
+    /**
+     *
+     * Constructor of class
+     */
+    public function _construct()
+    {
+        $this->view = new View();
+        $this->personModel = new personModel();
+    }
+    /**
+     *
+     * Gets the values from respective model class and passes the view class by 
+     * calling its specific function and passing data.
+     * 
+     * @access public
+     */
+    public function updateView()
+    {
+        
+    }
+    /**
+     *
+     * authenticates the user using person Model Class
+     * 
+     * @access public
+     * 
+     * @param string $username 'Username of Person'
+     * 
+     *        string $password 'Password of Person'
+     */
+    public function authenicateUserfromModel($username, $password)
+    {
+        
+        
+    }
+}
