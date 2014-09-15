8cd1ac03f27351a756d03b711748e57f381f5f21
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-09-15 13:15:20 +0500

    Add prototype of DB_Adapter includes - docblocks - its Singelton class as decided

---
+++ code/DB_Adapter.php
@@ -0,0 +1,70 @@
+<?php
+namespace Ready2Serve;
+/*
+ * This file contains DB_Adapter class
+ * 
+ * This file contains the implementions of DB_Adapter class that we need to
+ * deal with our database
+ * 
+ * @package Ready2Serve
+ * @version 1.0
+ */
+
+/*
+ * deals with database
+ * 
+ * This is singleton class deal with database by creating a single connection
+ * with database and use it.
+ * 
+ * @package Ready2Serve
+ * @author Tayyab Hussain <tayyab.hussain@coeus-solutions.de>
+ * @version 1.0
+ */
+class DB_Adapter
+{
+    
+    /*
+     * @var instance of DB_Adapter class
+     */
+    private $dbAdapter;
+    /*
+     * instantiate the DB_Adapter class
+     * 
+     * the constructor of DB_Adapter class is private because DB_Adapter is
+     * singleton class and not to be instantiate from any other class
+     */
+    private function __construct()
+    {
+        
+    }
+    /*
+     * returns the object of DB_Adapter class
+     * 
+     * this static function checks if the instance of DB_Adapter class is NULL
+     * then return the new object and if not returns the already created object
+     *
+     * @return DB_Adapter instance object of DB_Adapter class
+     */
+    public static function getInstance()
+    {
+        if ($this->dbAdapter== NULL) {
+            $this->dbAdapter=new DB_Adapter();
+            return $this->dbAdapter;
+        }
+        return $this->dbAdapter;
+    }
+    /*
+     * fetches all the records from database
+     * 
+     * this function fetches all the records from the database on the basis of 
+     * sql query
+     *
+     * @param  String $query sql query 
+     * 
+     * @return array  array containing the resultset
+     */
+    public function fetchAll($query)
+    {
+        
+    }
+}
