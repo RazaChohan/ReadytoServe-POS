d184a5b257b593fb1b0abb93616b29600680cd49
Muhammad Raza <muhammad.raza@coeus-solutions.de>
2014-09-15 17:29:34 +0500

    Add Code Prototype of Abstract Controller Class Contains: Functions for Abstract Controller Class Short Description, access level, return and parameter of functions Data Members of Controller and their description

---
+++ code/lib/Controller.php
@@ -0,0 +1,41 @@
+<?php
+namespace ready2Serve\lib;
+/*
+ * This file contains implementation of Abstract Controller Class
+ * 
+ * @package Ready2Serve
+ * @version 1.0
+ */
+
+/**
+ * Defines the basic functionality of controllers in an abstract
+ *
+ * @package Ready2Serve
+ * @author  Muhammad Raza <muhammad.raza@coeus-solutions.de>
+ * @version 1.0
+ */
+abstract class Controller {
+   /**
+   * sets the request for the controller
+   *
+   * @param  string $request 'array of strings containg controller path'
+   *
+   * @access public
+   */
+   public function setRequest($request)
+   {
+       
+   }
+   /**
+   * Checks for the pre-requisite conditions before calling the specific 
+   * action from the controller 
+   *
+   * @param  string $request array of strings containg controller path
+   *
+   * @access public
+   */
+   public function init()
+   {
+       
+   }
+}
