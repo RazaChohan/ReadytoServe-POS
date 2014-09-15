70fed46f732823a1451f3099c1b19a317365d812
jamshad.ahmad@coeus-solutions.de <jamshad.ahmad@coeus-solutions.de>
2014-09-15 15:20:40 +0500

    implementaion of View
    - one function added

---
+++ code/views/View.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ready2Serve\views\View;
+
+/*
+ * This file contains implementation of View
+ * 
+ * @package ready2Serve
+ * @version 1.0
+ */
+
+/**
+ * Prints required view script on console
+ *
+ * @package ready2Serve
+ * @author  Jamshad Ahmad <jamshad.ahmad@coeus-solutions.de>
+ * @version 1.0
+ */
+class View {
+  /**
+  * Gets name of view script and executes it
+  *
+  * @param string $viewName name of view script
+  *
+  * @access public
+  */
+  public function view($viewName)
+  {
+      
+  }
+}
