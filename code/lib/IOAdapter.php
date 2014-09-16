3d116ad8651a6113f1f794829c713f58cb0bdbd5
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-09-16 16:30:19 +0500

    IOAdapter add contains - getInput() to get input from user

---
+++ code/lib/IOAdapter.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file contains implementation of IOAdapter class
+ * 
+ * @package Ready2Serve
+ * @version 1.0
+ */
+
+/*
+ * prompts user to input
+ * 
+ * This is singleton class that asks user to input something and send back that
+ * input to caller.
+ * 
+ * @package Ready2Serve
+ * @author Tayyab Hussain <tayyab.hussain@coeus-solutions.de>
+ * @version 1.0
+ */
+class IOAdapter{
+   /*
+    * @var IOAdapter $handler the signle object of IOAdapter
+    */
+    private static $handler;
+    /*
+     * prompts user to input
+     * 
+     * this function prompts user to input something and return back that input
+     * to the caller
+     * 
+     * @return String input that has been recieved from user
+     */
+    public function getInput(){
+        $input=fgets(STDIN);
+        return $input;
+    }
+    /*
+     * returns the object of IOAdapter class
+     * 
+     * this static function checks if the instance of IOAdapter class is NULL
+     * then return the new object and if not returns the already created object
+     *
+     * @return IOAdapter instance object of IOAdapter class
+     */
+    public static function getInstance(){
+        if(self::$handler==NULL){
+            self::$handler=new IOAdapter();
+        }
+        return self::$handler;
+    }
+    
+}
+
