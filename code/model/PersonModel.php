e6a9c6c100602ba175722e3a9555cb911b5dd35c
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-09-16 18:21:21 +0500

    Update Person model change includes: - add authenticateUser()

--- code/model/PersonModel.php
+++ code/model/PersonModel.php
@@ -2,5 +2,19 @@
 
 class PersonModel
 {
-
+    public function authenticateUser($userName,$passWord){
+        $dba= DB_Adapter::getInstance();
+        $con=$dba->getConnection();
+        if($con){
+        	$query='SELECT username, DES_DECRYPT(password) from person'; 
+            $result= $dba->fetchAll($query);
+            while($row = mysqli_fetch_array($result)) {
+            	//echo $row['username'] . $row['DES_DECRYPT(password)'];
+            	if(trim($userName)===$row['username'] && trim($passWord)===$row['DES_DECRYPT(password)']){
+            		return true;
+            	}
+			}
+			return false;
+        }
+    }
 }
