c710e911b72517d5a1f640a397cf590870b0fccb
jamshad.ahmad@coeus-solutions.de <jamshad.ahmad@coeus-solutions.de>
2014-09-16 12:48:02 +0500

    view script added
    - welcome view
    - getPassword view
    - error screen

---
+++ code/views/error.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "".PHP_EOL;
+echo "--- Error : .$argv[1]. ---".PHP_EOL;
+echo "".PHP_EOL;
+
+
---
+++ code/views/welcome.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "".PHP_EOL;
+echo "-------------------------------------------------".PHP_EOL;
+echo "---------- Welcome to 'Ready To Server' ---------".PHP_EOL;
+echo "-------------------------------------------------".PHP_EOL;
+echo "".PHP_EOL;
+echo " >> Please Enter Your User Name : ";
