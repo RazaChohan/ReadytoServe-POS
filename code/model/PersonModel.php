88c669c44d2b7e3e51f3135513b468f4549d6123
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-09-17 10:02:25 +0500

    Update PersonModel change inlcudes: - refine docblocks

--- code/model/PersonModel.php
+++ code/model/PersonModel.php
@@ -1,48 +1,68 @@
 <?php
-/**
-* Contains implementation of PersonModel class.
-* 
-* This file contains the implementaion of PersonModel class
-* @category Training/Learning PHP
-* @package Ready2Serve
-* @version v 1.0
-*/
-/**
- * Contains Oauth Controller class
- *
- * This Class gets a specific request from the Dispatcher class
- * and performs functions according to the request.
+    
+/*
+ * Contains implementation of PersonModel class.
+ * 
+ * This file contains the implementaion of PersonModel class
+ * @category Training/Learning PHP
+ * @package Ready2Serve
+ * @version v 1.0
+ */
+/*
+ * perfoms Person's actions and maps to the table in DB
+ * 
+ * This class performs action related to the Person model and maps the
  * 
  * @package Ready2Serve
- * @author Muhammad Raza <muhammad.raza@coeus-solutions.de>
+ * @author Tayyab Hussain <tayyab.hussain@coeus-solutions.de>
  * @category Training/Learning PHP
  * @version v 1.0
  */
+
 class PersonModel
 {
+    /*
+     * @var string $personType to store type of person admin/salesman
+     */
     private static $personType=null;
-    
+    /*
+     * @var Boolean $isAuthenticated to keep the record about authentication
+     */
     private $isAuthenticated;
-    
+    /*
+     * returns the type of person
+     * 
+     * this is a static function which returns the type of person which can be
+     * either salesman or admin
+     * 
+     * @return string $personType type of person
+     */
     public static function getPersonType()
     {
         return self::$personType;
     }
-    
+    /*
+     * authenticates the person
+     * 
+     * this function takes username and password and authenticate if username
+     * and password is valid or not.
+     * 
+     * @return Boolen true if user valid otherwise false
+     */    
     public function authenticateUser($userName, $passWord)
     {
         $userName = trim($userName);
         $passWord = trim($passWord);
-
+            
         $dba = DB_Adapter::getInstance();
         $con = $dba->getConnection();
         if ($con) {
-
+            
             $query = "SELECT person_type, username, DES_DECRYPT(password)"
                     . "from person where username='" . $userName . "'"
                     . "AND DES_DECRYPT(password)='" . $passWord . "'";
-
-
+                        
+                        
             $result = $dba->fetchAll($query);
             $row = mysqli_fetch_array($result);
             if(count($row)>0)
