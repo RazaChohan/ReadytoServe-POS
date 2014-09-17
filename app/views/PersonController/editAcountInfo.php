de7f20b8fe4f9ddba19d691f87ab4f58c329ba82
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-09-17 17:28:01 +0500

    Add editAcountInfo view script also deletes adminMainMenu view script (extra)

---
+++ app/views/PersonController/editAcountInfo.php
@@ -0,0 +1,9 @@
+<?php
+$rg = Registry::getInstance();
+echo "".PHP_EOL;
+echo "-------------------------------------------------".PHP_EOL;
+echo "---------- Change Password Screen ---------".PHP_EOL;
+echo "-------------------------------------------------".PHP_EOL;
+echo "-----Logged In as UserName : ".$rg->logedInUserName."---".PHP_EOL;
+echo "-------------------------------------------------".PHP_EOL;
+echo "".PHP_EOL;
