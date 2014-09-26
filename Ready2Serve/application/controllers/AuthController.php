d70b093f1c63d4b4449bcf6f42ddb8a9ecb93096
Muhammad Raza <muhammad.raza@coeus-solutions.de>
2014-09-26 10:10:55 +0500

    Add Login Functionality 	modified:   .zfproject.xml    - New Form Added 	modified:   application/configs/application.ini    - Database settings Added 	modified:   application/controllers/AuthController.php    - Defined login Action 	new file:   application/forms/Login.php    - Added login Form 	modified:   application/layouts/scripts/layout.phtml 	modified:   application/views/scripts/auth/login.phtml    - Called login form

--- Ready2Serve/application/controllers/AuthController.php
+++ Ready2Serve/application/controllers/AuthController.php
@@ -2,24 +2,52 @@
 
 class AuthController extends Zend_Controller_Action
 {
-
     public function init()
     {
         /* Initialize action controller here */
     }
-
     public function indexAction()
     {
-        // action body
+       
     }
+    protected function _process($values)
+    {
+// Get our authentication adapter and check credentials
+        $adapter = $this->_getAuthAdapter();
+        $adapter->setIdentity($values['username']);
+        $adapter->setCredential($values['password']);
 
+        $auth = Zend_Auth::getInstance();
+        $result = $auth->authenticate($adapter);
+        if ($result->isValid()) {
+            $user = $adapter->getResultRowObject();
+            $auth->getStorage()->write($user);
+            return true;
+        }
+        return false;
+    }
     public function loginAction()
     {
+         // action body
+        $loginForm = new Application_Form_Login();
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if ($loginForm->isValid($request->getPost())) {
+                
+            }
+        }
+        $this->view->form = $loginForm;
         // action body
     }
+     protected function _getAuthAdapter()
+    {
+        $dbAdapter = Zend_Db_Table::getDefaultAdapter();
+        $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
 
+        $authAdapter->setTableName('person')
+                    ->setIdentityColumn('username')
+                    ->setCredentialColumn('password');
 
+        return $authAdapter;
+    }
 }
-
-
-
