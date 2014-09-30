e0ab30a3cd31f6bfe295553df03b6fd6381ef5d7
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-09-30 12:03:57 +0500

    add Person model

--- Ready2Serve/application/controllers/PersonController.php
+++ Ready2Serve/application/controllers/PersonController.php
@@ -16,12 +16,16 @@ class PersonController extends Zend_Controller_Action
     public function editAccountInfoAction()
     {
         // action body
-        $form=  new Application_Form_EditAccountInfo();
-        $this->view->form=$form;
+        $form = new Application_Form_EditAccountInfo();
+        $this->view->form = $form;
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $personModel = new Application_Model_Person();
+                $personModel->editAccountInfo($form->getValues());
+                $this->view->values = $form->getValues();
+            }
+        }
     }
 
-
 }
-
-
-
