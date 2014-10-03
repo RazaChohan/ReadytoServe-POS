bef3741c1ca5292a296f44a20c2880d86dc75691
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-10-03 09:27:35 +0500

    update place order

--- Ready2Serve/application/forms/PlaceOrder.php
+++ Ready2Serve/application/forms/PlaceOrder.php
@@ -16,28 +16,28 @@ class Application_Form_PlaceOrder extends Twitter_Form
         $pM = new Application_Model_Product();
         $products = $pM->getAllProducts();
         foreach ($products as $prod) {
-            $checkBox = $this->createElement('checkbox', $prod['product_id']);
-            if($prod['is_product_available']=='yes'){
+            if ($prod['is_product_available'] == 'yes') {
+                $checkBox = $this->createElement('checkbox', $prod['product_id']);
                 array_push($this->checkBoxArray, $checkBox);
+                $fieldQ = $this->createElement('text', $prod['product_id'] . 'q', array(
+                    'placeholder' => 'Quantity',
+                    'required' => true,
+                    'filters' => array('StringTrim'),
+                    'validators' => array(
+                        array('StringLength', false, array(1, 2))),
+                    'attribs' => array('disabled' => 'disabled')));
+                array_push($this->quantityArray, $fieldQ);
+                $fieldD = $this->createElement('text', $prod['product_id'] . 'd', array(
+                    'placeholder' => 'Discount',
+                    'required' => true,
+                    'filters' => array('StringTrim'),
+                    'validators' => array(
+                        array('StringLength', false, array(1, 2))),
+                    'attribs' => array('disabled' => 'disabled')));
+                array_push($this->discountArray, $fieldD);
+                array_push($this->productArray, $prod['product_description']);
+                array_push($this->priceArray, $prod['product_price']);
             }
-            $fieldQ = $this->createElement('text', $prod['product_id'] . 'q',array(
-                                            'placeholder'      => 'Quantity',
-                                            'required'   => true,
-                                            'filters'    => array('StringTrim'),
-                                            'validators' => array(
-                                            array('StringLength', false, array(1, 2))),
-                                            'attribs'    => array('disabled' => 'disabled')));
-            array_push($this->quantityArray, $fieldQ);
-            $fieldD = $this->createElement('text', $prod['product_id'] . 'd',array(
-                                            'placeholder'      => 'Discount',
-                                            'required'   => true,
-                                            'filters'    => array('StringTrim'),
-                                            'validators' => array(
-                                            array('StringLength', false, array(1, 2))),
-                                            'attribs'    => array('disabled' => 'disabled')));
-            array_push($this->discountArray, $fieldD);
-            array_push($this->productArray, $prod['product_description']);
-            array_push($this->priceArray, $prod['product_price']);
         }
     }
 
